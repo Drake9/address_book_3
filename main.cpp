2ffcf3ac0e0b7d4b7d8dfbb92ec080885a84549e
Marcin Zapala <marcin.zapala.programista@gmail.com>
2019-02-21 19:38:52 +0100

    wyswietlanie menu

--- main.cpp
+++ main.cpp
@@ -1,9 +1,41 @@
 #include <iostream>
+#include <windows.h>
+#include <conio.h>
+#include <cstdlib>
+#include <fstream>
+#include <vector>
 
 using namespace std;
 
-int main()
-{
-    cout << "Hello world!" << endl;
+void wypiszMenuLogowania(){
+    system("cls");
+	cout << "KSIAZKA ADRESOWA" << endl << endl;
+	cout << "1. Logowanie" << endl;
+	cout << "2. Rejestracja" << endl << endl;
+	cout << "0. Zamknij program" << endl << endl;
+}
+
+void wypiszMenuGlowne(){
+	system("cls");
+	cout << "KSIAZKA ADRESOWA" << endl << endl;
+	cout << "1. Dodaj adresata" << endl;
+	cout << "2. Wyszukaj po imieniu" << endl;
+	cout << "3. Wyszukaj po nazwisku" << endl;
+	cout << "4. Wyswietl wszystkich adresatow" << endl;
+	cout << "5. Usun adresata" << endl;
+	cout << "6. Edytuj adresata" << endl << endl;
+	cout << "8. Zmien haslo" << endl << endl;
+	cout << "0. Wyloguj sie" << endl << endl;
+}
+
+int main(){
+    bool zalogowany = false;
+    int liczbaKontaktow = 0;
+    char wybor = 0;
+
+    while(!zalogowany){
+        wypiszMenuLogowania();
+        wybor = getch();
+    }
     return 0;
 }
